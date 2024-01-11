bee486e4234b1323e5b1e7e8753be0241c299aca
Agile <labhishettysaikalyan@gmail.com>
2024-01-11 11:28:56 +0530

    Done with task4

--- src/components/TaskFour.vue
+++ src/components/TaskFour.vue
@@ -1,13 +1,32 @@
 <template>
-    <div>
-      <p>Passing data from child component to parent component using emit.</p>
-      <button @click="sendDataToParent">Send Data to Parent</button>
-    </div>
-  </template>
+   <div>
+    <TaskFourChild
+        title="Passing data from child to parent"
+        content="On clicking the button you can see data processed from child component"
+        @displayData="fetchData($event)">
+    </TaskFourChild>
+    <TaskFourChild
+        title="This is another example of passing data from child to parent"
+        content="Similarly on clicking get data from child you can see data processed from child"
+        @displayData="fetchData($event)">
+    </TaskFourChild>
+   </div>
+   <div>
+    <h3>Result : <span style="color: rgb(255, 219, 164);text-align: center;font-size: 24px;">{{ result }}</span></h3>
+   </div>
+</template>
   
-  <script setup>
-  const sendDataToParent = () => {
-    context.emit('childToParent', 'Hello from Child!');
-  };
-  </script>
+<script setup>
+  import {ref} from 'vue'
+  import TaskFourChild from './TaskFourChild.vue'
+  const result = ref("This is default result")
+  const fetchData = (event) => {
+    result.value = event;
+  }
+</script>
   
+<style>
+h3{
+  text-align: center;
+}
+</style>
---
+++ src/components/TaskFourChild.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="data">
+        <h1> {{ title }}</h1>
+        <h4> {{ content }}</h4>
+        <button @click="loadData">Get data from child</button>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['title','content'])
+const emit = defineEmits('displayData')
+
+const loadData = () => {
+    emit('displayData',props.content)
+}
+
+</script>
+
+<style>
+.data > h4{
+    text-align: center;
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+.data button{
+    margin-left: 45%;
+}
+
+</style>
